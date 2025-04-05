014279057b49f491c78d5020e458365f2f786521
kouki223 <kouki.1818194@gmail.com>
2025-04-06 05:32:10 +0900

    テスト駆動型で機能の実装

--- test/convert_length_test.rb
+++ test/convert_length_test.rb
@@ -3,6 +3,10 @@ require_relative '../lib/convert_length'
 
 class ConvertLengthTest < Minitest::Test
     def test_convert_length
-        assert_equal 39.37, convert_length(1, 'm', 'in')
+        assert_equal 39.37, convert_length(1, from: :m, to: :in)
+        #インチからメールへ変換する
+        assert_equal 0.38, convert_length(15, from: :in, to: :m)
+        #フィートからメートルに変換
+        assert_equal 10670.73, convert_length(35000, from: :ft, to: :m)
     end
 end
